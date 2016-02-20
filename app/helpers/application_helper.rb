7942a38f9e505871069454e8ee9f24d6fe724902
lesliep <hi@lesliephifer.com>
2016-02-19 19:54:04 -0500

    Style flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def alert_classes alert_type
+    alert = {
+      :success => "alert-success",
+      :error => "alert-error",
+      :alert => "alert-warning"
+    }[alert_type] || "alert-info"
+
+    "alert #{alert} alert-dismissible"
+  end
 end
