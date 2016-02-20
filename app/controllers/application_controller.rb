b6b5c18aff0690414204e882023ca5b08ea91b61
lesliep <hi@lesliephifer.com>
2016-02-19 23:40:57 -0500

    Rescue from access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include Monban::ControllerHelpers
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = exception.message
+    redirect_to root_path
+  end
 end
