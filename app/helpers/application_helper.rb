d862754936f8180ee2767a5d4a4577e8ee5f5147
lesliep <hi@lesliephifer.com>
2016-02-26 23:11:05 -0500

    Rubocopped helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
-
-  def alert_classes alert_type
+  def alert_classes(alert_type)
     alert = {
-      :success => "alert-success",
-      :error => "alert-error",
-      :alert => "alert-warning"
-    }[alert_type] || "alert-info"
+      success: 'alert-success',
+      error: 'alert-error',
+      alert: 'alert-warning'
+    }[alert_type] || 'alert-info'
 
     "alert #{alert} alert-dismissible"
   end
--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,22 +1,22 @@
 module ProfileHelper
-
-  def unverified_social_media_button provider
+  def unverified_social_media_button(provider)
     if SocialMediaAccount.all_providers.include? provider
-      link_to "Add #{provider}", 
-        social_media_link(provider),
-        :class => "btn btn-default social_media_account #{provider} unverified"
+      link_to "Add #{provider}",
+              social_media_link(provider),
+              class: "btn btn-default social_media_account \
+              #{provider} unverified"
     end
   end
 
-  def verified_social_media_button account
+  def verified_social_media_button(account)
     link_to "#{account.provider}: #{account.username}",
             account.url,
-           :target => "_blank",
-           :class => "btn btn-default social_media_account #{account.provider} verified"
-
+            target: '_blank',
+            class: "btn btn-default social_media_account \
+            #{account.provider} verified"
   end
 
-  def social_media_link provider
+  def social_media_link(provider)
     "/auth/#{provider}"
   end
 end
